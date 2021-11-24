cc7a37198d8975fe6f84381c643d1a2d39fabb69
Ella Frost <ded_19@hotmail.com>
2021-11-24 14:02:15 +0100

    New with results

--- delfinen/src/Compatition.java
+++ delfinen/src/Compatition.java
@@ -1,2 +1,13 @@
 public class Compatition {
+    private String name;
+    //it might be better to have a result class with WHAT member, the result and their time. While competition is the actual competition itself.
+    private double competitionTime;
+    private Member[] competitors;
+
+    public Compatition(String name, double competitionTime)
+    {
+        this.name = name;
+        this.competitionTime = competitionTime;
+    }
 }
+
