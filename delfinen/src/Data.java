0dcc11ae2f1523bf0748490cb63598aa2df08fee
balt0012 <90677412+balt0012@users.noreply.github.com>
2021-12-10 04:09:49 +0100

    reader and writer finalised kinda...

--- delfinen/src/Data.java
+++ delfinen/src/Data.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Data {
@@ -31,7 +34,6 @@ public class Data {
         }
         try {
             FileWriter writer = new FileWriter("members/" + newMember.getName() + ".csv");
-            //toString is to be added
             writer.write(newMember.toString());
             writer.close();
             System.out.println("new member has been added!");
@@ -62,7 +64,99 @@ public class Data {
     }
 
     public static ArrayList<Member> getMembers() {
+        int memberAmount = 0;
+        String[] memberNames = new String[1];
+        ArrayList<Member> newMembers = new ArrayList<>();
+        try{
+            File file = new File("members/names.csv");
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                memberAmount ++;
+                String name = sc.nextLine();
+                //System.out.println(name);
+            }
+            sc = new Scanner(file);
+            memberNames = new String[memberAmount];
+            for (int i = 0; i < memberAmount; i++) {
+                memberNames[i] = sc.nextLine();
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Could not find names");
+        }
+        try{
+            for (int i = 0; i < memberAmount; i++) {
+                File file = new File("members/" + memberNames[i] + ".csv");
+                Scanner sc = new Scanner(file);
+                String priceYearly = sc.nextLine();
+                double priceYearlyInt = Double.parseDouble(priceYearly);
+                String priceYearlyIsPaid = sc.nextLine();
+                boolean priceYearlyIsPaidBoolean;
+                if (priceYearlyIsPaid.equals("true")){
+                    priceYearlyIsPaidBoolean = true;
+                } else {
+                    priceYearlyIsPaidBoolean = false;
+                }
+                String isActive = sc.nextLine();
+                boolean isActiveBoolean;
+                if (isActive.equals("true")){
+                    isActiveBoolean = true;
+                } else {
+                    isActiveBoolean = false;
+                }
+                String age = sc.nextLine();
+                int ageInt = Integer.parseInt(age);
+                String memberId = sc.nextLine();
+
+                String name = sc.nextLine();
+                String isPricePaid = sc.nextLine();
+                boolean isPricePaidBoolean;
+                if (isPricePaid.equals("true")){
+                    isPricePaidBoolean = true;
+                } else {
+                    isPricePaidBoolean = false;
+                }
+                Member member = new Member(priceYearlyInt, priceYearlyIsPaidBoolean, isActiveBoolean, ageInt, memberId, name, isPricePaidBoolean);
+                newMembers.add(member);
+                sc.close();
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Could not find names");
+        }
+        setMembers(newMembers);
+
         return members;
+    }
+
+    public static ArrayList<String> getNames(){
+        int memberAmount = 0;
+        ArrayList<String> memberNames = new ArrayList<>();
+        try{
+            File file = new File("members/names.csv");
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                memberAmount ++;
+                String name = sc.nextLine();
+                //System.out.println(name);
+            }
+            sc = new Scanner(file);
+            for (int i = 0; i < memberAmount; i++) {
+                memberNames.add(sc.nextLine());
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Could not find names");
+        }
+        return memberNames;
+    }
+
+    public static void setNames(String names){
+        try {
+            FileWriter writer = new FileWriter("members/names.csv");
+            writer.write(names);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
     }
 
     public static void setMembers(ArrayList<Member> members) {
--- delfinen/src/Member.java
+++ delfinen/src/Member.java
@@ -19,6 +19,17 @@ public class Member {
         setPriceYearly();
     }
 
+    public Member(double priceYearly, boolean priceYearlyIsPaid, boolean isActive, int age, String memberID, String name, boolean isPricePaid) {
+        this.priceYearly = priceYearly;
+        this.priceYearlyIsPaid = priceYearlyIsPaid;
+        this.isActive = isActive;
+        this.age = age;
+        this.memberID = memberID;
+        this.name = name;
+        this.isPricePaid = isPricePaid;
+
+    }
+
     public void setPriceYearly() {
         // sets price based on active status and age.
         if (!isActive) {
@@ -66,6 +77,14 @@ public class Member {
         return isActive;
     }
 
+    public void setPricePaid(boolean pricePaid) {
+        isPricePaid = pricePaid;
+    }
+
+    public void setPriceYearlyIsPaid(boolean priceYearlyIsPaid) {
+        this.priceYearlyIsPaid = priceYearlyIsPaid;
+    }
+
     public boolean PriceYearlyIsPaid() {
         return priceYearlyIsPaid;
     }
@@ -102,40 +121,50 @@ public class Member {
 
         Member newMember = new Member(age, id, isActive, name);
         Data.addToMembers(newMember);
+        ArrayList<String> newNames = Data.getNames();
+        newNames.add(newMember.getName());
+        String newNamesString = "";
+        for (int i = 0; i < newNames.toArray().length; i++) {
+            newNamesString = newNamesString + newNames.toArray()[i] + "\n";
+        }
+        Data.setNames(newNamesString);
     }
 
     public static void editMember(){
-        //WIP
         Scanner scanner = new Scanner(System.in);
         System.out.println("which member do you want to edit:");
         for (int i = 0; i < Data.getMembers().toArray().length; i++) {
             System.out.println("\n" + (i+1) + " " + Data.getMembers().toArray()[i].toString());
         }
-        Member memberToEdit = (Member) Data.getMembers().toArray()[scanner.nextInt()];
-        System.out.println("what do you want to edit: 1.active 2.age 3.name");
+        Member memberToEdit = (Member) Data.getMembers().toArray()[scanner.nextInt()-1];
+        System.out.println(memberToEdit.getName());
+        System.out.println("what do you want to edit: 1.active 2.age 3.price paid");
         int varToEdit = scanner.nextInt();
         do {
             if (varToEdit == 1){
                 if(memberToEdit.getIsActive()){
                     memberToEdit.setActive(false);
-                    System.out.println("member activity switched");
                 } else {
                     memberToEdit.setActive(true);
-                    System.out.println("member activity switched");
                 }
+                System.out.println("member activity switched");
             }else if (varToEdit == 2){
                 System.out.println("what is the age of the member?");
                 int age = scanner.nextInt();
                 memberToEdit.setAge(age);
             }else if (varToEdit == 3){
-                System.out.println("what is the name of the member?");
-                String name = scanner.nextLine();
-                memberToEdit.setName(name);
+                if(memberToEdit.isPricePaid){
+                    memberToEdit.setPricePaid(false);
+                    memberToEdit.setPriceYearlyIsPaid(false);
+                } else {
+                    memberToEdit.setPricePaid(true);
+                    memberToEdit.setPriceYearlyIsPaid(true);
+                }
             }
-        } while (varToEdit != 0);
+        } while (varToEdit != 0 && varToEdit != 1 && varToEdit != 2 && varToEdit != 3);
+        memberToEdit.setPriceYearly();
         Data.addToMembers(memberToEdit);
     }
-
 
     @Override
     public String toString() {
